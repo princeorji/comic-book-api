c50183cd189997dcdf2f390fd53a5a9fc490925b
Michael Orji <princemikeorji@gmail.com>
2022-11-17 23:46:52 +0100

    models setup

--- config/settings.py
+++ config/settings.py
@@ -92,6 +92,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Artist(models.Model):
+    name = models.ForeignKey('User', on_delete=models.CASCADE)
+    biography = models.CharField(max_length=225)
+    is_currently_employed = models.BooleanField()
+
+    def __str__(self):
+        return self.name.get_username()
+
+class Series(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Issue(models.Model):
+    name = models.CharField(max_length=25)
+    issue_number = models.CharField(max_length=25)
+    publication_date = models.DateField()
+    artist_id = models.ForeignKey('Artist', on_delete=models.CASCADE)
+    series_id = models.ForeignKey('Series', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
