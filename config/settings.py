89827f9cba18e92dd2c480a314432450ac81ff60
Michael Orji <princemikeorji@gmail.com>
2022-11-23 21:23:21 +0100

    deployment-ready

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
